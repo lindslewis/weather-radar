b136ad8aa1a8c43abe342754baf76a0a3133b0e8
LindsayLew <lmschwehr@gmail.com>
2022-07-16 16:04:17 -0700

    waiting for the key to activate

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@
 // below is my button for searching, need to call it here so I can attach the function to it 
 const searchButton = document.querySelector('#searchButton');
 
+
 // lat and lon are required and for the latitude and longitude of place being called on
 function getAPI() {
-    const requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=ec4f51ab549c402396d72795f8b68224"
+    // below is an example for PDX
+    const requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=45.51&lon=-122.67&appid=ec4f51ab549c402396d72795f8b68224";
+    // const requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=ec4f51ab549c402396d72795f8b68224";
 
     fetch(requestUrl)
         .then(function (response) {
