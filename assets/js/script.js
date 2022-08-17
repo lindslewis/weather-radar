b36168c4c8e20f598a5b94a445ab9c8145f9bc45
LindsayLew <lmschwehr@gmail.com>
2022-08-17 15:34:16 -0700

    restructure, wasn't doing an async

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,39 @@
+const requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=45.51&lon=-122.67&appid=ec4f51ab549c402396d72795f8b68224";
 
 // below is my button for searching, need to call it here so I can attach the function to it 
 const searchButton = document.querySelector('#searchButton');
 
 searchButton.addEventListener('click', getAPI);
 // lat and lon are required and for the latitude and longitude of place being called on
-function getAPI() {
-    // below is an example for PDX
-    const requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=45.51&lon=-122.67&appid=ec4f51ab549c402396d72795f8b68224";
+async function getAPI(url) {
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-        console.log(data)
-            // for (let i = 0; i <data.length; i++) {
-            //     const listItem = document.create
-            // }
-        });
+    const response = await fetch(url);
+
+    let data = await response.json();
+    console.log(data);
+    if (response) {
+        // not sure if the below is necessary
+        hideLoader()
+    }
+    show(data);
 }
 
-getAPI();
+getAPI(requestUrl);
+
+
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//         console.log(data)
+//             // for (let i = 0; i <data.length; i++) {
+//             //     const listItem = document.create
+//             // }
+//         });
+// }
+
+// getAPI();
 
 // searchButton.addEventListener('click', getAPI);
 
