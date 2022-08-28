e4ea2b50527f4c2fe7384db2a03f1e9269d0d280
LindsayLew <lmschwehr@gmail.com>
2022-08-27 22:22:16 -0700

    here we go

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,38 @@
 
-// above needs to take the user input in the curlies... 
+// // above needs to take the user input in the curlies... 
 
-// below is my button for searching, need to call it here so I can attach the function to it 
-const searchButton = document.querySelector('#searchButton');
+// // below is my button for searching, need to call it here so I can attach the function to it 
+// const searchButton = document.querySelector('#searchButton');
 
-searchButton.addEventListener('click', getAPI);
-// lat and lon are required and for the latitude and longitude of place being called on
-async function getAPI(url) {
+// searchButton.addEventListener('click', getAPI);
+// // lat and lon are required and for the latitude and longitude of place being called on
+// async function getAPI(url) {
 
-    const response = await fetch(url);
+//     const response = await fetch(url);
 
-    let data = await response.json();
-    console.log(data);
-    if (response) {
-        return console.log('weather api has been searched');
+//     let data = await response.json();
+//     console.log(data);
+//     if (response) {
+//         return console.log('weather api has been searched');
 
-        // not sure if the below is necessary
-        // hideLoader()
-    }
-    show(data);
-}
-for ( let i = 0; i < data.length; i++) {
-    console.log(data[i].name)
-}
-// for innerhtml stuff, to show on the page
-async function show() {
-    // name of city, replaceChild() for this one perhaps?
-    const cityName = document.querySelector(".cityName");
-    document.body.appendChild(cityName);
-    // double check 27 below
-    const obj = parser(await requestUrl()); 
-    cityName.innerHTML = JSON.stringify(obj, null, 2);
-}
-getAPI(requestUrl);
+//         // not sure if the below is necessary
+//         // hideLoader()
+//     }
+//     show(data);
+// }
+// for ( let i = 0; i < data.length; i++) {
+//     console.log(data[i].name)
+// }
+// // for innerhtml stuff, to show on the page
+// async function show() {
+//     // name of city, replaceChild() for this one perhaps?
+//     const cityName = document.querySelector(".cityName");
+//     document.body.appendChild(cityName);
+//     // double check 27 below
+//     const obj = parser(await requestUrl()); 
+//     cityName.innerHTML = JSON.stringify(obj, null, 2);
+// }
+// getAPI(requestUrl);
 
 
 //     fetch(requestUrl)
@@ -69,7 +69,18 @@ getAPI(requestUrl);
 // each bit of the 5day forecast gives date, icon for weather conditions, temp, windspeed, humidity
 
 // search history is populated from the local storage, can click and research
+const cityInputElem = document.querySelector("#userInput");
 
+// we are going to want to edgecase for all lowercase, uppercase, etc
 const formSubmitHandler = function (e) {
+    e.preventDefault();
 
-}
+    // this needs to call the input from the html
+    const cityName = cityInputElem.value.trim();
+
+    if(cityName) {
+        // call function here that searches the api???
+    } else {
+        alert("invalid: please enter a city name")
+    }
+};
